d0925fc99662dac0e964de6386ffd25cc97821f3
shagun Sodhani <sshagunsodhani@gmail.com>
2016-10-23 00:53:51 +0530

    Added module to read input file and word-vectors file

---
+++ app/reader/filereader.py
@@ -0,0 +1,41 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import numpy as np
+
+
+def read_glove_vectors(glove_vector_path):
+    '''Method to read glove vectors and return an embedding dict.'''
+    embeddings_index = {}
+    with open(glove_vector_path) as f:
+        for line in f:
+            values = line.strip().split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs[:]
+    return embeddings_index
+
+
+def read_input_data(input_data_path):
+    '''Method to read data from input_data_path'''
+    texts = []  # list of text samples
+    labels_index = {}  # dictionary mapping label name to numeric id
+    labels = []  # list of label ids
+
+    with open(os.path.join(input_data_path, "input.txt"), 'rb') as sentence_f:
+        for line in sentence_f:
+            sentence = str(line.strip())
+            texts.append(sentence)
+
+    with open(os.path.join(input_data_path, "label.txt"), 'rb') as class_f:
+        largest_label_id = 0
+        for line in class_f:
+            label = str(line.strip())
+            if label not in labels_index:
+                labels_index[label] = largest_label_id
+                largest_label_id += 1
+            labels.append(labels_index[label])
+
+    return texts, labels_index, labels
